e08b46b55c397d48bf92d00969e6155e01947a67
HiMashuguptA <sg707012@gmail.com>
2024-03-15 17:08:14 +0530

    Updated Schema

--- Backend/Schema/Schema.js
+++ Backend/Schema/Schema.js
@@ -3,6 +3,31 @@ const Schema = new mongoose.Schema({
     Name:{
         type:String,
         required:true
+    },
+    Date:{
+        type:Date,
+        required:true
+    },
+    Guests:{
+        type: String
+    },
+    Venue:{
+        type:Data,
+        required:true
+    },
+    Image:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    Description:{
+        type:String
+    },
+    Organiser:{
+        type:String
     }
 })
 
