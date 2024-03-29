9a4a48af5d0875403eceeacbf56438fc0d834150
HiMashuguptA <sg707012@gmail.com>
2024-03-29 11:57:04 +0530

    Added animation

--- Front-End/src/Components/Blog/BlogPage.jsx
+++ Front-End/src/Components/Blog/BlogPage.jsx
@@ -1,36 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Reusable_comp/Navbar';
 import Footer from '../Reusable_comp/Footer';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import '../../App.css'
 
 const BlogPage = () => {
+    const [visible, setIsVisible] = useState(true);
     const handleClick = () => {
         window.scrollBy({
             top: 900,
             behavior: 'smooth'
         });
     };
+    useEffect(()=>{
+        Aos.init({duration:1000})
+        const handleScroll = () => {
+            if (window.scrollY > 30) {
+                setIsVisible(false);
+            } else {
+                setIsVisible(true);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    },[])
   return (
     <main>
         <Navbar/>
         <div className="bg-[url(/Blog_page_img/bg.jpg)] bg-no-repeat bg-cover bg-center bg-fixed z-10 -mt-24">
         <div className="flex justify-center h-screen align-middle items-center pb-28 flex-col">
-                    <div>
+                    <div data-aos="fade-zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine">
                         <p className="text-gray-400 text-8xl max-sm:text-5xl">Post Your</p>
                         <p className="text-Royal-Golden text-10xl font-Lavishly_Yours ml-44 max-sm:text-7xl max-sm:ml-16">Happiness</p>
                     </div>
-                    <div>
+                    <div className={visible ? 'symbol-visible' : 'symbol-hidden'}>
                         <div className="mt-40 -mb-80 animate-bounce flex flex-col justify-center max-sm:mt-60 " >
                             <img src="/BodyImg/symbol.svg" alt="Gt Symbol" className="cursor-pointer" onClick={handleClick} loading='lazy' />
                             <img src="/BodyImg/symbol.svg" alt="Gt Symbol" className="cursor-pointer" onClick={handleClick} loading='lazy'/>
                         </div>
                     </div>
-                    <div className="fixed right-8 bottom-28 max-sm:right-5 max-sm:bottom-14">
+                    <div className="fixed right-8 bottom-28 max-sm:right-5 max-sm:bottom-14" data-aos="fade-zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine">
                         <img src="/Blog_page_img/comment.png" alt="add post" className="w-20 h-20 invert cursor-pointer max-sm:w-14 max-sm:h-14" loading='lazy' />
                     </div>
                 </div>
                 <Footer />
-                <div className="flex flex-col justify-between">
-                    <div className="h-72 w-3/5 bg-pappy-brown outline-dotted outline-offset-8 rounded-sm outline-Royal-Golden self-center mt-96 max-sm:h-56 max-sm:w-80">
+                <div className="flex flex-col justify-between pb-36">
+                    <div className="h-72 w-3/5 bg-pappy-brown outline-dotted outline-offset-8 rounded-sm outline-Royal-Golden self-center mt-96 max-sm:h-56 max-sm:w-80" data-aos="fade-zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine">
                         <div className="flex">
                             <div className="flex flex-col h-72 justify-evenly pl-9 w-4/5 max-sm:flex-row max-sm:pt-4">
                                 <img src="/Blog_page_img/profile-1.jpg" alt="Profile photo" className="h-48 w-44 rounded-full border-2 max-sm:h-16 max-sm:w-16" loading='lazy' />
@@ -52,8 +72,8 @@ const BlogPage = () => {
                             </div>
                         </div>
                     </div>
-                    <hr className="w-4/5 self-center mt-20" />
-                    <div className="h-72 w-3/5 bg-pappy-brown outline-dotted outline-offset-8 rounded-sm outline-Royal-Golden self-center mt-20 max-sm:h-56 max-sm:w-80 max-sm:mt-16">
+                    <hr className="w-4/5 self-center mt-20" data-aos="fade-zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine"/>
+                    <div className="h-72 w-3/5 bg-pappy-brown outline-dotted outline-offset-8 rounded-sm outline-Royal-Golden self-center mt-20 max-sm:h-56 max-sm:w-80 max-sm:mt-16" data-aos="fade-zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine">
                         <div className="flex">
                             <div className="flex flex-col h-72 justify-evenly pl-9 w-4/5 max-sm:flex-row max-sm:pt-4">
                                 <img src="/Blog_page_img/profile-1.jpg" alt="Profile photo" className="h-48 w-44 rounded-full border-2 max-sm:h-16 max-sm:w-16" loading='lazy' />
@@ -75,8 +95,8 @@ const BlogPage = () => {
                             </div>
                         </div>
                     </div>
-                    <hr className="w-4/5 self-center max-sm:mt-16 mt-20" />
-                    <div className="h-72 w-3/5 bg-pappy-brown outline-dotted outline-offset-8 rounded-sm outline-Royal-Golden self-center mt-20 max-sm:h-56 max-sm:w-80 max-sm:mt-16">
+                    <hr className="w-4/5 self-center max-sm:mt-16 mt-20" data-aos="fade-zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine"/>
+                    <div className="h-72 w-3/5 bg-pappy-brown outline-dotted outline-offset-8 rounded-sm outline-Royal-Golden self-center mt-20 max-sm:h-56 max-sm:w-80 max-sm:mt-16" data-aos="fade-zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine">
                         <div className="flex">
                             <div className="flex flex-col h-72 justify-evenly pl-9 w-4/5 max-sm:flex-row max-sm:pt-4">
                                 <img src="/Blog_page_img/profile-1.jpg" alt="Profile photo" className="h-48 w-44 rounded-full border-2 max-sm:h-16 max-sm:w-16" loading='lazy' />
@@ -99,12 +119,6 @@ const BlogPage = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div><p>asdaad</p></div> */}
-                {/* <div><p>asdaad</p></div> */}
-
-                {/* <div><p>asdaad</p></div> */}
-
-                {/* <div><p>asdaad</p></div> */}
 
         </div>
     </main>
