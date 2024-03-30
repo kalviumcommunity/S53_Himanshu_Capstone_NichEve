d053d2e82de8c520d3e472555397e06a0620a143
HiMashuguptA <sg707012@gmail.com>
2024-03-30 10:09:46 +0530

    added loading and alt in img

--- Front-End/src/Components/Contact_us/Contact.jsx
+++ Front-End/src/Components/Contact_us/Contact.jsx
@@ -51,15 +51,15 @@ const Contact = () => {
                 <Footer />
                 <div className="flex justify-center">
                     <div className="flex justify-around mt-64 pb-64 max-sm:flex-col ">
-                        <img src="/Contact_us_Img/profile_pic.jpg" alt="contact_us_pic" className="w-96 h-52 rounded-xl border-2 border-[#FF00F5] max-sm:h-28 max-sm:w-40 max-sm:self-center" />
+                        <img src="/Contact_us_Img/profile_pic.jpg" alt="contact_us_pic" loading='lazy' className="w-96 h-52 rounded-xl border-2 border-[#FF00F5] max-sm:h-28 max-sm:w-40 max-sm:self-center" />
                         <div className="flex flex-col h-72 justify-evenly -mt-16 w-96 max-sm:h-40 max-sm:mt-0 max-sm:w-auto">
                             <p className="text-3xl text-white w-[30rem] text-center ml-14 max-sm:text-sm max-sm:w-60 max-sm:ml-0">Hey, its me Himanshu Gupta and if you have any query you can contact me via- </p>
                             <div className="flex max-sm:ml-0 ml-14 max-sm:self-center">
-                                <span className="self-center"><img src="/Contact_us_Img/Mail.svg" alt="Mail" className="max-sm:w-7" /></span>
+                                <span className="self-center"><img src="/Contact_us_Img/Mail.svg" alt="Mail" loading='lazy' className="max-sm:w-7" /></span>
                                 <span className="text-white text-2xl ml-4 -mt-1 max-sm:ml-2 max-sm:text-sm max-sm:mt-0">Sg707012@gmail.com</span>
                             </div>
                             <div className="flex ml-14 max-sm:self-center max-sm:ml-0 max-sm:mr-16">
-                                <span className="self-center"><img src="/Contact_us_Img/Phone.svg" alt="Mail" className="max-sm:w-6" /></span>
+                                <span className="self-center"><img src="/Contact_us_Img/Phone.svg" alt="phone_dialer" loading='lazy' className="max-sm:w-6" /></span>
                                 <span className="text-white text-2xl ml-4 -mt-1 max-sm:ml-2 max-sm:text-sm max-sm:mt-0">9366496765</span>
                             </div>
                         </div>
