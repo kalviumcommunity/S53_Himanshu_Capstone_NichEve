81be8de83f30cc8976a6bab05b6c9234093490cd
HiMashuguptA <sg707012@gmail.com>
2024-04-09 18:20:38 +0530

    Fixed minor errors

--- Backend/route.js
+++ Backend/route.js
@@ -2,35 +2,35 @@ const express = require('express')
 const app = express();
 const router = express.Router();
 const {isConnected} = require('./Database/Db')
-const Event = require('./Schema/Schema')
+// const Event = require('./Schema/Schema')
 const BForm = require('./Schema/BlogSchema')
 app.use(express.json())
 router.get('/',(req,res)=>{
     res.send(`Database Connection Status: ${isConnected()? 'Connection Successful' : 'Connection Unsuccessful'}`)
 })
-router.get('/Events',async(req,res)=>{
-    try {
-        const AllEvents = await Event.find();
-        res.send({Events:AllEvents})
-    } catch (error) {
-        console.error("Error occurred while fetching events:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
+// router.get('/Events',async(req,res)=>{
+//     try {
+//         const AllEvents = await Event.find();
+//         res.send({Events:AllEvents})
+//     } catch (error) {
+//         console.error("Error occurred while fetching events:", error);
+//         res.status(500).json({ error: "Internal server error" });
+//     }
     
-})
+// })
 
-router.post('/Events',async(req,res)=>{
-    try {
-        const data = req.body;
-        const new_event_data = new Event(data);
-        const response = await new_event_data.save();
-        console.log("Data Saved");    
-        res.status(200).json(response)    
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({error:"Internal server error"})
-    }
-})
+// router.post('/Events',async(req,res)=>{
+//     try {
+//         const data = req.body;
+//         const new_event_data = new Event(data);
+//         const response = await new_event_data.save();
+//         console.log("Data Saved");    
+//         res.status(200).json(response)    
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json({error:"Internal server error"})
+//     }
+// })
 
 
 router.post('/PostBlog', async(req,res)=>{
@@ -39,10 +39,10 @@ router.post('/PostBlog', async(req,res)=>{
         const new_Blog_Post = new BForm(data);
         const response = await new_Blog_Post.save();
         console.log("Data Saved");
-        res.send(200).json(response);
+        return res.status(200).json(response);
     }catch(err){
         console.log(err);
-        res.send(500).json({error:"internal server error"});
+        return res.status(500).json({error:"internal server error"});
     }
 })
 router.get('/PostBlog',async(req,res)=>{
@@ -51,7 +51,7 @@ router.get('/PostBlog',async(req,res)=>{
         res.send({Posts:AllPosts})
     } catch (error) {
         console.log(error);
-        res.status(500).json({error:"Internal server error"})
+        return res.status(500).json({error:"Internal server error"})
     }
 })
 
